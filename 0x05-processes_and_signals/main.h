587a66d0724d09aef785d3f8fdab21172e21e61c
youssefbassem42 <62960010+youssefbassem42@users.noreply.github.com>
2023-12-23 00:20:43 +0200

    Done !

---
+++ 0x05-processes_and_signals/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
+int _putchar(char c);
+#endif MAIN_H
